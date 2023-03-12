e1823d48ac51d494ad39650497ca89434abc7b9f
Александр Плохой <alexplohoy@mail.ru>
2023-03-12 18:23:28 +0300

    add functions overview

--- synopsis.js
+++ synopsis.js
@@ -57,7 +57,7 @@ for (let num of numArray) {
     console.log(num)
 }
 
-// objects and functions
+// objects
 function someFun(name, age, arg2, arg3) {
     // to do
 }
@@ -122,17 +122,3 @@ console.log(Number(bigNuber) + 12)
 console.log(bigNuber + BigInt(12))
 // console.log(bigNuber + 12) // error
 
-// string
-const str = `Name: ${name} and age: ${age} doing ${getDoing(age)}`
-
-function getDoing(num) {
-    return num + num
-}
-
-function printStr(s, arg1, arg2) {
-    console.log(`${s[0]}${arg1}${s[1]}${arg2}${s[2]}`)
-}
-
-console.log(str)
-
-const output = printStr`String with ${num} and with ${age}`
