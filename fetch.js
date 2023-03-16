b0af7206f41142225cddfd1f7cb3fd2bd7cf12e8
Александр Плохой <alexplohoy@mail.ru>
2023-03-16 11:14:59 +0300

    add post fetch other

--- fetch.js
+++ fetch.js
@@ -25,15 +25,41 @@ function sendRequest(method, url, body = null) {
     })
 }
 
+function sendFetchRequest(method, url, body = null) {
+    const headers = {
+        "Content-Type": "application/json"
+    }
+
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }).then(response => {
+        if (response.ok) {
+            return response.json()
+        }
+
+        return response.json().then(error => {
+            const e = new Error("Something is wrong")
+            e.data = error
+            throw e
+        })
+    })
+}
+
 // sendRequest("GET", requestUrl)
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
-
 
 const body = {
     name: "Alex",
     age: 26
 }
+
 sendRequest("POST", requestUrl, body)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+sendFetchRequest("POST", requestUrl, body)
     .then(data => console.log(data))
     .catch(err => console.log(err))
