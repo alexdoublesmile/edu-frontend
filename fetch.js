e5ff26f44e9dafdcfe7f60920a7a357d10eaaaea
Александр Плохой <alexplohoy@mail.ru>
2023-03-16 10:37:44 +0300

    add post fetch

--- fetch.js
+++ fetch.js
@@ -1,12 +1,13 @@
 const requestUrl = "https://jsonplaceholder.typicode.com/users"
 
-function sendRequest(method, url) {
+function sendRequest(method, url, body = null) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
         
         xhr.open(method, url)
     
         xhr.responseType = "json"
+        xhr.setRequestHeader("Content-Type", "application/json")
     
         xhr.onload = () => {
             if (xhr.status >= 400) {
@@ -20,10 +21,19 @@ function sendRequest(method, url) {
             reject(xhr.response)
         }
     
-        xhr.send()
+        xhr.send(JSON.stringify(body))
     })
 }
 
-sendRequest("GET", requestUrl)
+// sendRequest("GET", requestUrl)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+
+const body = {
+    name: "Alex",
+    age: 26
+}
+sendRequest("POST", requestUrl, body)
     .then(data => console.log(data))
     .catch(err => console.log(err))
