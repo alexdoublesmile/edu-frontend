7bc8acdb9a5f091d74b45cab478faf5b78c31a7f
Александр Плохой <alexplohoy@mail.ru>
2023-03-16 10:32:08 +0300

    add get fetch

---
+++ fetch.js
@@ -0,0 +1,29 @@
+const requestUrl = "https://jsonplaceholder.typicode.com/users"
+
+function sendRequest(method, url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        
+        xhr.open(method, url)
+    
+        xhr.responseType = "json"
+    
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                reject(xhr.response)
+            } else {
+                resolve(xhr.response)
+            }
+        }
+    
+        xhr.onerror = () => {
+            reject(xhr.response)
+        }
+    
+        xhr.send()
+    })
+}
+
+sendRequest("GET", requestUrl)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
