a12ea91797c1ab556049441e8724b97e75da49e6
Александр Плохой <alexplohoy@mail.ru>
2023-03-18 17:28:12 +0300

    add reload plugins config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import { babel } from "@rollup/plugin-babel";
 import styles from "rollup-plugin-styles";
 import image from "rollup-plugin-img";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
 
 export default {
     input: "./src/index.js",
@@ -11,6 +13,12 @@ export default {
     plugins: [
         babel({ babelHelpers: "bundled" }),
         styles(),
-        image({ limit: 300000 })
+        image({ limit: 300000 }),
+        serve({
+            open: true,
+            contentBase: "./",
+            port:8000
+        }),
+        livereload()
     ]
 };
