a0120f77d9106feb863d34d6b14b3a57f3a27a26
Александр Плохой <alexplohoy@mail.ru>
2023-03-18 10:42:52 +0300

    add siple scripts

---
+++ index.js
@@ -0,0 +1,48 @@
+const mainNavigation = document.querySelector(".main-navigation");
+const firstNavButton = document.querySelector(".main-navigation__button-item");
+const navButtons = document.querySelectorAll(".main-navigation__button-item");
+const deleteButtons = document.querySelectorAll(".task-item__delete-button");
+console.log(mainNavigation)
+
+console.log(typeof mainNavigation);
+
+deleteButtons.forEach(button => button.textContent = "Remove...");
+// deleteButtons.forEach(button => button.innerHTML = "");
+
+const taskForm = document.querySelector("create-task-block");
+// console.log(taskForm.children);
+
+const firstButton = document.querySelector(".task-item__delete-button");
+console.log(firstButton.style);
+// firstButton.style.background = "yellow";
+
+
+// create Element
+const newNavButton = document.createElement("a");
+newNavButton.className = "main-navigation__button-item";
+newNavButton.href = "#tasks_expired";
+newNavButton.dataset.buttonId = "4";
+newNavButton.textContent = "Expired tasks";
+console.log(newNavButton);
+
+// add Element
+// mainNavigation.append(newNavButton);
+// mainNavigation.insertAdjacentElement("afterbegin", newNavButton);
+
+// remove element
+// mainNavigation.remove();
+
+// attributes
+const createTaskInput = document.querySelector(".create-task-block__input");
+// createTaskInput.hasAttribute("type");
+// createTaskInput.getAttribute("type");
+// createTaskInput.removeAttribute("type");
+// createTaskInput.setAttribute("type", "text");
+
+// listeners
+navButtons.forEach(button => button.addEventListener("click", event => {
+    navButtons.forEach(button => button.classList.remove("main-navigation__button-item_selected"));
+    // const { target } = event;
+    const target = event.target;
+    target.classList.add("main-navigation__button-item_selected");
+}));
