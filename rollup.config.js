7a2b8aa75eb086e745e40067bc635cc46d07868d
Александр Плохой <alexplohoy@mail.ru>
2023-03-18 16:02:10 +0300

    add babel to config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import { babel } from "@rollup/plugin-babel";
+
 export default {
     input: "./index.js",
     output: {
         file: "./build/bundle.js",
         format: "iife"
-    }
-}
+    },
+    plugins: [
+        babel({ babelHelpers: "bundled" })
+    ]
+};
