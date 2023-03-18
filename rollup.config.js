fe2cc54eb819497a73dc5fb5eabb1863662caa00
Александр Плохой <alexplohoy@mail.ru>
2023-03-18 16:34:51 +0300

    add img plugin config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { babel } from "@rollup/plugin-babel";
 import styles from "rollup-plugin-styles";
+import image from "rollup-plugin-img";
 
 export default {
     input: "./src/index.js",
@@ -9,6 +10,7 @@ export default {
     },
     plugins: [
         babel({ babelHelpers: "bundled" }),
-        styles()
+        styles(),
+        image({ limit: 10000 })
     ]
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "../index.css";
+import LOGO_IMG from "../assets/logo.png";
 
 console.log("Hi, there");
 
 const imgHTML = document.createElement("img");
 imgHTML.classList.add("logo-img");
-imgHTML.src = "../assets/logo.png";
+imgHTML.src = "LOGO_IMG";
 
 document.body.append(imgHTML);
