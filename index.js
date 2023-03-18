aaa8146234753d363c92e67b9dac036fb6caf0b8
Александр Плохой <alexplohoy@mail.ru>
2023-03-18 11:59:03 +0300

    add listeners

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const mainNavigation = document.querySelector(".main-navigation");
 const firstNavButton = document.querySelector(".main-navigation__button-item");
 const navButtons = document.querySelectorAll(".main-navigation__button-item");
+
+const taskForm = document.querySelector(".create-task-block");
+const taskInput = document.querySelector(".create-task-block__input");
+
+const firstButton = document.querySelector(".task-item__delete-button");
 const deleteButtons = document.querySelectorAll(".task-item__delete-button");
 console.log(mainNavigation)
 
@@ -9,10 +14,8 @@ console.log(typeof mainNavigation);
 deleteButtons.forEach(button => button.textContent = "Remove...");
 // deleteButtons.forEach(button => button.innerHTML = "");
 
-const taskForm = document.querySelector("create-task-block");
 // console.log(taskForm.children);
 
-const firstButton = document.querySelector(".task-item__delete-button");
 console.log(firstButton.style);
 // firstButton.style.background = "yellow";
 
@@ -33,11 +36,10 @@ console.log(newNavButton);
 // mainNavigation.remove();
 
 // attributes
-const createTaskInput = document.querySelector(".create-task-block__input");
-// createTaskInput.hasAttribute("type");
-// createTaskInput.getAttribute("type");
-// createTaskInput.removeAttribute("type");
-// createTaskInput.setAttribute("type", "text");
+// taskInput.hasAttribute("type");
+// taskInput.getAttribute("type");
+// taskInput.removeAttribute("type");
+// taskInput.setAttribute("type", "text");
 
 // listeners
 navButtons.forEach(button => button.addEventListener("click", event => {
@@ -47,8 +49,7 @@ navButtons.forEach(button => button.addEventListener("click", event => {
     target.classList.add("main-navigation__button-item_selected");
 }));
 
-const createForm = document.querySelector(".create-task-block");
-createForm.addEventListener("submit", event => {
+taskForm.addEventListener("submit", event => {
     console.log(event);
     event.preventDefault();
     
@@ -57,11 +58,13 @@ createForm.addEventListener("submit", event => {
     console.log(taskName);
 });
 
-// const createForm = document.querySelector(".create-task-block");
 document.addEventListener("keydown", event => {
+    console.log("keydown");
+});
+
+document.addEventListener("keyup", event => {
     const key = event.key;
-    // console.log(event);
-    console.log(key);
+    console.log("keyup");
     
     const task = document.querySelector(`[data-task-id="${key}"]`);
     if (task) {
@@ -69,5 +72,54 @@ document.addEventListener("keydown", event => {
         if (deleteConfirmed) {
             task.remove();
         }
+    }
+});
+
+const createTooltip = text => {
+    const tooltip = document.createElement("span");
+    tooltip.textContent = text;
+    tooltip.className = "tooltip";
+
+    return tooltip;
+}
+
+deleteButtons.forEach(button => button.addEventListener("mouseover", event => {
+    const target = event.target;
+    // console.log(target);
+
+    const taskItemHTML = target.closest(".task-item");
+    const taskId = taskItemHTML?.dataset.taskId;
+    if (taskId) {
+        const tooltipHTML = createTooltip(`Remove ${taskId}?`);
+        target.append(tooltipHTML);
+    }
+}));
+
+deleteButtons.forEach(button => button.addEventListener("mouseout", event => {
+    const target = event.target;
+    const tooltip = document.querySelector(".tooltip");
+    tooltip.remove();
+}));
+
+const checkInputNotValid = value => {
+    if (!value || value.includes("@")) {
+        return true;
+    }
+    return false;
+};
+
+taskInput.addEventListener("input", event => {
+    const target = event.target;
+    const value = target.value;
+    const notValid = checkInputNotValid(value);
+    const errorMsg = document.querySelector(".error-message-block");
+
+    if (notValid) {
+        const errorMessage = document.createElement("span");
+        errorMessage.classList.add("error-message-block");
+        errorMessage.textContent = "Error text! Not empty and no @!"
+        taskForm.append(errorMessage);
+    } else if (!notValid && errorMsg) {
+        errorMsg.remove();
     }
 });
