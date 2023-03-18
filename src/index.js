a99b872348ba83061bcadc0c261229dcb745b060
Александр Плохой <alexplohoy@mail.ru>
2023-03-18 16:26:59 +0300

    add img by js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "../index.css";
 
 console.log("Hi, there");
+
+const imgHTML = document.createElement("img");
+imgHTML.classList.add("logo-img");
+imgHTML.src = "../assets/logo.png";
+
+document.body.append(imgHTML);
