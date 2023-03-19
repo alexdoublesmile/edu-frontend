b5d2ec978d1545f15986887c53b4bd24584cb597
Александр Плохой <alexplohoy@mail.ru>
2023-03-19 10:39:44 +0300

    new project webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,44 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-
-const path = require("path");
-
-module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "build.js",
-        path: path.resolve(__dirname, "build")
-    },
+module.exports = ({ development }) => ({
+    mode: development ? 'development' : 'production',
+    entry: `${__dirname}/index.js`,
     devServer: {
-        static: "./build",
-        port: 8080,
-        open: true
+        contentBase: `${__dirname}/dist`,
+        open: true,
+        compress: true,
+        port: 8000,
     },
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
     module: {
         rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['@babel/preset-env', { targets: "defaults" }]
-                ]
-              }
-            }
-          },
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-              },
-            ],
-          }
-        ]
-      },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./index.html"
-      }),
-      new CleanWebpackPlugin()
-    ]
-}
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+});
