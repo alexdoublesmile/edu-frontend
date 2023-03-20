c7a7defc93659ec4cba04c9c03804eb1b118b04f
Александр Плохой <alexplohoy@mail.ru>
2023-03-20 20:00:53 +0300

    fix index html

--- webpack_version_full/index.js
+++ webpack_version_full/index.js
@@ -1,8 +1,5 @@
 import "./index.css";
-// import { App as AppComponent } from "./src/modules/app";
 import App from "./src/modules/app";
 
 const app = new App();
 app.run();
-
-console.log("start")
