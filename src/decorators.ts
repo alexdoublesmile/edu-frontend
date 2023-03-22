d0f8e45baa0f32ae4ce8a990e594964a8ae96e95
Александр Плохой <alexplohoy@mail.ru>
2023-03-22 09:09:11 +0300

    add 4 decorator types

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,9 +1,54 @@
-function Log(constructor: Function) {
+function ClassDecorator(constructor: Function) {
+    console.log("---------- ClassDecorator -----------")
+    console.log(`constructor: ${constructor}`)
     console.log(constructor);
 }
 
-@Log
-class Component {
+function FieldDecorator(target: any, propName: string | Symbol) {
+    console.log("---------- FieldDecorator ------------")
+    console.log(`target: ${target}`)
+    console.log(target)
+    console.log(`propName: ${propName}`)
+    console.log(propName)
+}
 
+function FunctionDecorator(target: any, propName: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log("---------- FunctionDecorator ------------")
+    console.log(`target: ${target}`)
+    console.log(target)
+    console.log(`propName: ${propName}`)
+    console.log(propName)
+    console.log(`descriptor: ${descriptor}`)
+    console.log(descriptor)
+}
 
+function AccessorDecorator(target: any, propName: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log("---------- AccessorDecorator ------------")
+    console.log(`target: ${target}`)
+    console.log(target)
+    console.log(`propName: ${propName}`)
+    console.log(propName)
+    console.log(`descriptor: ${descriptor}`)
+    console.log(descriptor)
+}
+
+@ClassDecorator
+class TestComponent {
+    @FieldDecorator
+    name: string
+
+    @AccessorDecorator
+    get coponentName(): string {
+        return this.name
+    }
+
+    constructor(name: string) {
+        this.name = name
+    }
+    
+    @FunctionDecorator
+    logName(): void {
+        console.log("----------- Inside invocation ----------------")
+        console.log(`TestComponent name ${this.name}`)
+    }
 }
