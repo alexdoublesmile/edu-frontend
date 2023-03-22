0a03af68515abb69c3841ad45a221d235151f1f5
Александр Плохой <alexplohoy@mail.ru>
2023-03-22 18:03:26 +0300

    add angular component

--- src/angular.ts
+++ src/angular.ts
@@ -1,13 +1,40 @@
-function Component(constructor: any) {
-    
+
+
+interface ComponentDecorator {
+    selector: string
+    template: string
 }
 
-@Component
-class AngularComponent {
-    constructor(public name: string) {}
-    
-    logName(): void {
-        console.log("----------- Inside invocation ----------------")
-        console.log(`TestComponent name ${this.name}`)
+function Component(config: ComponentDecorator) {
+    return function
+        <T extends { new(...args: any[]): object}>
+    (Constructor: T) {
+        return class extends Constructor {
+            constructor(...args: any[]) {
+                super(...args)
+
+                const element = document.querySelector(config.selector)!
+                element.innerHTML = config.template
+            }
+        }
     }
 }
+
+@Component({
+    selector: "#card",
+    template: `
+        <div class="card">
+            <div class="card-title">
+                <div class="card-details">
+                    Card Coponent
+                </div>
+            </div>
+        </div>
+    `
+})
+class CardComponent {
+    constructor(public name: string) {
+    }
+}
+
+const card = new CardComponent("Card Component");
